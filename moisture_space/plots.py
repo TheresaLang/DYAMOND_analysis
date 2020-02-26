4db99806dea3eb78231fe33f244388fb4d7be0db
TheresaLang <theresa_lang@gmx.net>
2020-02-26 17:04:44 +0100

    Contour levels for moisture space contour plots are now explicitly specified instead of number of contours.

--- moisture_space/plots.py
+++ moisture_space/plots.py
@@ -6,10 +6,10 @@ import seaborn
 import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
 
-def moisture_space_contourf(fig, ax, x_vector, y_vector, plot_field, num_contours, x_lims, y_lims, x_label, y_label, cb_label, **kwargs):
+def moisture_space_contourf(fig, ax, x_vector, y_vector, plot_field, contours, x_lims, y_lims, x_label, y_label, cb_label, **kwargs):
     
     x, h = np.meshgrid(x_vector, y_vector)
-    im = ax.contourf(x, h, plot_field, num_contours, **kwargs)
+    im = ax.contourf(x, h, plot_field, contours, **kwargs)
     cb = fig.colorbar(im, orientation='horizontal', ax=ax)
     cb.set_label(cb_label)
     ax.set_xlim(x_lims[0], x_lims[1])
