e8d1b0ba82476120a10810e185aa4a7308ccdda5
TheresaLang <theresa_lang@gmx.net>
2020-03-08 17:49:54 +0100

    Add methods to copy objects, return attributes, interpolate profiles on new vertical levels and remove bins with statistics based on a few profiles only.

--- moisture_space/moisture_space.py
+++ moisture_space/moisture_space.py
@@ -1,5 +1,6 @@
 import utils
 import numpy as np
+import matplotlib.pyplot as plt
 from scipy.interpolate import interp1d
 
 class MoistureSpace:
@@ -24,9 +25,22 @@ class MoistureSpace:
         
         if self.profile_stats.mean.shape[0] != self.num_profiles:
             self.profile_stats.transpose()
+            
+    def copy(self):
+        """ Create a new instance of MoistureSpace, with the same data as this instance.
+        """
+        return MoistureSpace(self.profile_stats, self.bins, self.levels)
+    
+    @property
+    def mean(self):
+        """ Returns the mean profiles, i.e. the attribute profile_stats.mean.
+        """
+        return self.profile_stats.mean
         
     def value_at_level(self, level):
-        """ Returns values of the variable (mean) at a given level and all bins.
+        """ Returns values of the variable (mean) at any given level and all bins.
+        Linear interpolation between descrete levels is performed to determine
+        the value at the given level.
         
         Parameters:
             level (float): Level
@@ -64,8 +78,24 @@ class MoistureSpace:
     
     def interpolate(self, levels):
         """ Interpolate profile statistics vertically to given levels.
+        
+        Parameters:
+            levels (array): Levels to interpolate on
+            
+        Returns: 
+            MoistureSpace: New instance containing interpolated fields.
         """
-        return None
+        ret = self.copy()
+        #interpolate
+        for s in vars(self.profile_stats):
+            field = getattr(self.profile_stats, s)
+            if (field is not None) and (not isinstance(field, str)):
+                field_interp = np.ones((self.num_profiles, len(levels))) * np.nan
+                for p in range(self.num_profiles):
+                    field_interp[p] = interp1d(self.levels, field[p], fill_value='extrapolate',\
+                                               bounds_error=False)(levels)
+                ret.profile_stats.set_attr(s, field_interp)                      
+        return ret
         
 class PercMoistureSpace(MoistureSpace):
     """ Class storing mean atmospheric profiles in equally sized bins, i.e. bin statistics have
@@ -80,10 +110,30 @@ class PercMoistureSpace(MoistureSpace):
         """
         super().__init__(profile_stats, levels, bins)
         
+    def copy(self):
+        """ Create a new instance of MoistureSpace, with the same data as this instance.
+        """
+        return PercMoistureSpace(self.profile_stats, self.bins, self.levels)
+        
     def mean_profile(self, bin_start=None, bin_end=None):
+        """ Returns the mean over several bins.
+        
+        Parameters:
+            bin_start (float): First bin to use (default: None; first bin is used)
+            bin_end (float): Last bin to use (defualt: None; last bin is used)
+            
+        Returns:
+            array: Mean values of variable over all bins between bin_start and bin_end for each level.            
         """
-        """
-        return None
+        if bin_start is None:
+            bin_start = np.min(self.bins)
+        if bin_end is None:
+            bin_end = np.max(self.bins)
+        
+        bin_ind = np.logical_and(self.bins >= bin_start, self.bins <= bin_end)
+        bin_mean = np.mean(self.profile_stats.mean[bin_ind], axis=0)
+        
+        return bin_mean
         
 class BinMoistureSpace(MoistureSpace):
     """ Class storing mean atmospheric profiles in equally spaced bins, i.e. bin statistics have
@@ -100,6 +150,21 @@ class BinMoistureSpace(MoistureSpace):
         super().__init__(profile_stats, levels, bins)
         self.profile_pdf = profile_pdf
         
+    def copy(self):
+        """ Create a new instance of MoistureSpace, with the same data as this instance.
+        """
+        return BinMoistureSpace(self.profile_stats, self.bins, self.levels, self.profile_pdf)
+        
+    def remove_empty_bins(self, number_threshold):
+        """ Remove all bins with less profiles than ``number_threshold``.
+        
+        Parameters:
+            number_threshold (int): Minimum number of profiles in a bin
+        """
+        pass
+        empty_bins = np.where(self.profile_pdf <= number_threshold)[0]
+        self.profile_stats.remove_bins(empty_bins)
+                
 class ProfileStats:
     """ Class containing statistics of binned profiles. """
     def __init__(self, variable=None, mean=None, median=None, std=None, minimum=None, maximum=None):
@@ -138,12 +203,33 @@ class ProfileStats:
         
         return ret
     
+    def copy(self):
+        """ Create a new instance of ProfileStats, with the same data as this instance.
+        """
+        return ProfileStats(self.variable, self.mean, self.median, self.std, self.min, self.max)
+    
+    def set_attr(self, attribute, value):
+        """ Set an attribute. """
+        setattr(self, attribute, value)
+                                    
     def transpose(self):
         """ Transpose all attributes that are ndarrays.
         """
         for a in vars(self):
             s = getattr(self, a)
             if (s is not None) and (not isinstance(s, str)):
-                print(s.shape)
                 setattr(self, a, s.T)
+                
+    def remove_bins(self, bin_ind):
+        """ Set statistics of specified bins to NaN.
+        
+        Parameters:
+            bin_ind (array or list): Indices of bins to remove
+        """
+        for a in vars(self):
+            s = getattr(self, a)
+            if (s is not None) and (not isinstance(s, str)):
+                s_new = s.copy()
+                s_new[bin_ind] = np.nan
+                setattr(self, a, s_new)
         
