450824bd58a3a8d2e98d41b20d346632bd6825e4
TheresaLang <theresa_lang@gmx.net>
2020-03-11 18:03:41 +0100

    Add method to calculate an area mean to class PercMoistureSpace. Add class to collect several MoistureSpaces and calculate variability and EOFs.

--- moisture_space/moisture_space.py
+++ moisture_space/moisture_space.py
@@ -2,10 +2,11 @@ import utils
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.interpolate import interp1d
+from importlib import reload
 
 class MoistureSpace:
     """ Class storing atmospheric profiles in moisture space. """
-    def __init__(self, profile_stats=None, bins=None, levels=None):
+    def __init__(self, name=None, profile_stats=None, bins=None, levels=None):
         """ Create instance of class MoistureSpace.
         
         Parameters:
@@ -13,6 +14,7 @@ class MoistureSpace:
             bins (array): Bins (x-coordinate of moisture space, e.g. integrated water vapour)
             levels (array): Vertical levels (y-coordinate of moisture space, e.g. height)   
         """
+        self.name = name
         self.profile_stats = profile_stats  # (bins, levels)
         self.variable = profile_stats.variable
         self.levels = levels
@@ -29,7 +31,7 @@ class MoistureSpace:
     def copy(self):
         """ Create a new instance of MoistureSpace, with the same data as this instance.
         """
-        return MoistureSpace(self.profile_stats, self.bins, self.levels)
+        return MoistureSpace(self.name, self.profile_stats, self.bins, self.levels)
     
     @property
     def mean(self):
@@ -94,13 +96,15 @@ class MoistureSpace:
                 for p in range(self.num_profiles):
                     field_interp[p] = interp1d(self.levels, field[p], fill_value='extrapolate',\
                                                bounds_error=False)(levels)
-                ret.profile_stats.set_attr(s, field_interp)                      
+                ret.profile_stats.set_attr(s, field_interp)  
+        ret.levels = levels
+        ret.num_levels = len(levels)
         return ret
         
 class PercMoistureSpace(MoistureSpace):
     """ Class storing mean atmospheric profiles in equally sized bins, i.e. bin statistics have
     been calculated from an equal number of profiles in each bin (percentiles). """
-    def __init__(self, profile_stats=None, levels=None, bins=None):
+    def __init__(self, name=None, profile_stats=None, levels=None, bins=None):
         """ Create instance of class PercMoistureSpace,.
         
         Parameters:
@@ -108,12 +112,12 @@ class PercMoistureSpace(MoistureSpace):
             bins (array): Bins (x-coordinate of moisture space, e.g. integrated water vapour)
             levels (array): Vertical levels (y-coordinate of moisture space, e.g. height)   
         """
-        super().__init__(profile_stats, levels, bins)
+        super().__init__(name, profile_stats, levels, bins)
         
     def copy(self):
         """ Create a new instance of MoistureSpace, with the same data as this instance.
         """
-        return PercMoistureSpace(self.profile_stats, self.bins, self.levels)
+        return PercMoistureSpace(self.name, self.profile_stats, self.bins, self.levels)
         
     def mean_profile(self, bin_start=None, bin_end=None):
         """ Returns the mean over several bins.
@@ -134,11 +138,35 @@ class PercMoistureSpace(MoistureSpace):
         bin_mean = np.mean(self.profile_stats.mean[bin_ind], axis=0)
         
         return bin_mean
+    
+    def area_mean(self, bin_start=None, bin_end=None, lower_bnd=None, upper_bnd=None):
+        """ Calculate the average over a certain area in moisture space.
+        
+        Parameters:
+            bin_start (float): First bin to use (default: None; first bin is used)
+            bin_end (float): Last bin to use (defualt: None; last bin is used)
+        """
+        if bin_start is None:
+            bin_start = np.min(self.bins)
+        if bin_end is None:
+            bin_end = np.max(self.bins)            
+        if lower_bnd is None:
+            lower_bnd = np.min(self.levels)
+        if upper_bnd is None:
+            upper_bnd = np.max(self.levels)
+        
+        layer_ind = np.logical_and(self.levels >= lower_bnd, self.levels <= upper_bnd)
+        bin_ind = np.logical_and(self.bins >= bin_start, self.bins <= bin_end)
+        delta_lev = np.gradient(self.levels)
+        layer_mean = np.average(self.profile_stats.mean[:, layer_ind], axis=1, weights=delta_lev[layer_ind])       
+        area_mean = np.mean(layer_mean[bin_ind], axis=0)
+        
+        return area_mean
         
 class BinMoistureSpace(MoistureSpace):
     """ Class storing mean atmospheric profiles in equally spaced bins, i.e. bin statistics have
     been calculated from different numbers of profiles in each bin."""
-    def __init__(self, profile_stats=None, levels=None, bins=None, profile_pdf=None):
+    def __init__(self, name=None, profile_stats=None, levels=None, bins=None, profile_pdf=None):
         """ Create instance of class PercMoistureSpace,.
         
         Parameters:
@@ -147,13 +175,13 @@ class BinMoistureSpace(MoistureSpace):
             levels (array): Vertical levels (y-coordinate of moisture space, e.g. height)
             profile_pdf (array): Number of profiles in each bin (same length as ``bins``)
         """
-        super().__init__(profile_stats, levels, bins)
+        super().__init__(name, profile_stats, levels, bins)
         self.profile_pdf = profile_pdf
         
     def copy(self):
         """ Create a new instance of MoistureSpace, with the same data as this instance.
         """
-        return BinMoistureSpace(self.profile_stats, self.bins, self.levels, self.profile_pdf)
+        return BinMoistureSpace(self.name, self.profile_stats, self.bins, self.levels, self.profile_pdf)
         
     def remove_empty_bins(self, number_threshold):
         """ Remove all bins with less profiles than ``number_threshold``.
@@ -232,4 +260,68 @@ class ProfileStats:
                 s_new = s.copy()
                 s_new[bin_ind] = np.nan
                 setattr(self, a, s_new)
+                
+class MoistureSpaceCollection:
+    """ Class to calculate statistics over several moisture spaces, e.g. from several models.
+    """
+    def __init__(self, moisture_spaces):
+        """ Create instance of class MoistureSpaceCollection.
+        
+        Parameters:
+            moisture_spaces (list of MoistureSpace): List of MoistureSpace instances with 
+                identical bins and levels. 
+        """
+        self.moisture_spaces = moisture_spaces
+        self.num_spaces = len(moisture_spaces)
+        self.bins = moisture_spaces[0].bins
+        self.levels = moisture_spaces[0].levels
+        self.num_bins = len(self.bins)
+        self.num_levels = len(self.levels)
+        
+    @property
+    def space_array(self):
+        """ Concatenate all moisture spaces (means) of this collection to one array.
+        
+        Returns:
+            3darray: Array containing all moisture spaces of this collection
+        """
+        space_array = np.ones((self.num_spaces, self.num_bins, self.num_levels)) * np.nan
+        for n in range(self.num_spaces):
+            space_array[n] = self.moisture_spaces[n].mean
+        
+        return space_array
+    
+    @property
+    def variability(self):
+        """ Calculate variability among different moisture spaces.
+        """ 
+        return np.std(self.space_array, axis=0)
+    
+    def calc_EOFs(self):
+        """ Calculate Empirical Orthogonal Functions (EOFs) for the collection of different 
+        moisture spaces.
+        """
+        reload(utils)
+        reshaped_space_array = self.space_array.reshape((self.num_spaces, self.num_bins * self.num_levels))
+        eofs, variability_frac, expansion_coeffs = utils.eofs(reshaped_space_array)
+        eofs = eofs.reshape(self.num_bins, self.num_levels, self.num_bins * self.num_levels).transpose(2, 0, 1)
+        
+        self.eofs = eofs
+        self.variability_frac = variability_frac
+        self.expansion_coeffs = expansion_coeffs
+        
+        return eofs, variability_frac, expansion_coeffs
+    
+    def recunstruct_from_EOFs(self, num_EOFs):
+        """
+        """
+        eofs = self.eofs.transpose(1, 2, 0).reshape(self.num_bins * self.num_levels, self.num_bins * self.num_levels)
+        
+        #0: model 0
+        #rep = np.matmul(np.expand_dims(expansion_coeffs['RH'][0], 1), np.expand_dims(eofs['RH'][0].ravel(), 0))
+        #plt.imshow(np.real(rep[5].reshape(100, 50)))
+        
+        
+        
+       
         
