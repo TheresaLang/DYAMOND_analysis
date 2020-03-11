47f119ff0f4c67fe6de89682572831b5e06643f2
TheresaLang <theresa_lang@gmx.net>
2020-03-11 18:04:58 +0100

    Add argument to pass xticks and xticklabels to moisture_space_contourf.

--- moisture_space/plots.py
+++ moisture_space/plots.py
@@ -6,7 +6,7 @@ import seaborn
 import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
 
-def moisture_space_contourf(fig, ax, x_vector, y_vector, plot_field, contours, x_lims, y_lims, x_label, y_label, cb_label, **kwargs):
+def moisture_space_contourf(fig, ax, x_vector, y_vector, plot_field, contours, x_lims, y_lims, x_label, y_label, cb_label, ticks=None, tick_labels=None, **kwargs):
     
     x, h = np.meshgrid(x_vector, y_vector)
     im = ax.contourf(x, h, plot_field, contours, **kwargs)
@@ -16,6 +16,15 @@ def moisture_space_contourf(fig, ax, x_vector, y_vector, plot_field, contours, x
     ax.set_ylim(y_lims[0], y_lims[1])
     ax.set_xlabel(x_label)
     ax.set_ylabel(y_label)
+    
+    if ticks is not None:
+        tick_idx = []
+        for t in tick_labels:
+            tick_idx.append(np.argmin(np.abs(ticks - t)))
+        ax.set_xticks(tick_idx)
+        ax.set_xticklabels(tick_labels)
+        for label in ax.xaxis.get_ticklabels()[-2:]:
+            label.set_visible(False)
     
     return im
     
